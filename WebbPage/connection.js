54ba88021eef3f646f8af4bbd6bd9f333bf5b817
maxwinberg <winberg.max@gmail.com>
2015-02-03 12:35:34 +0100

    js ändring

--- WebbPage/connection.js
+++ WebbPage/connection.js
@@ -6,32 +6,12 @@ var io = require('socket.io')(http);
 
 
 
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('chat message', function(msg){
-    	io.emit('chat message', msg);
-		console.log(msg);
-  	});
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-});
-
-http.listen(3000, function(){
-	console.log('listening on *:3000');
-});
-
 function connect(){
 	socket = io.connect('http://op-en.se:5000');
 	socket.on('connect', function () {
 	    socket.on('mqtt', function (msg) {
 	      console.log(msg.topic+' '+msg.payload);
 	    });
-	    socket.emit('subscribe',{topic:'/some/sensor/data'});
+	    
 	}); 
 }
