88211ff97db13889e1d770b53efac8b409e52441
maxwinberg <winberg.max@gmail.com>
2015-02-03 12:52:37 +0100

    Js

--- WebbPage/connection.js
+++ WebbPage/connection.js
@@ -1,10 +1,5 @@
 
 var socket;
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-
 
 function connect(){
 	socket = io.connect('http://op-en.se:5000');
