bc6e9ef20cf1c34673a8f76d1770544c174a9920
maxwinberg <winberg.max@gmail.com>
2015-02-17 13:22:22 +0100

    In connection.js: added code for summering and converting the amount in watt to cost. Also started the template of droping coins.

--- webpage/js/connection.js
+++ webpage/js/connection.js
@@ -2,6 +2,8 @@
 var socket;
 var measurement_data = [];
 var start, end, time;
+var wattSum = 0;
+var totalCost = 0;
 
 function connect(source){
 	console.log("Tjena boys");
@@ -18,7 +20,7 @@ function connect(source){
 function measurement_storage(msg, date){
 
 	measurement_data.push(
-		{"measurement":[{"buildingwatt": msg.payload, "date": date}]}
+		{"buildingwatt": msg.payload, "date": date}
 		);
 }
 
@@ -28,8 +30,37 @@ function check(msg){
 		end = new Date().getTime();
 		time = end-start;
 		start = end;
-		measurement_storage(msg, new Date());
-		console.log(msg.topic+' '+msg.payload+', time elapsed:'+time+', Date:'+new Date());
-		console.log("size: " + measurement_data.length);
+		wattSum += msg.payload;
+
+		setTotalCost(money_converter(wattSum));
+		//measurement_storage(msg, new Date());
+		//console.log(msg.topic+' '+msg.payload+', time elapsed:'+time+', Date:'+new Date());
+		//console.log("size: " + measurement_data.length + " buildingwatt: " + measurement_data[measurement_data.length-1]["buildingwatt"] + " Date: " + measurement_data[measurement_data.length-1]["date"]);
 	}
 }
+
+//Sets the cost amount. It also checks weather of not the new cost has become greater that the previous total,
+//And starts the drop coins.
+function setTotalCost(cost){
+	if(Math.floor(totalCost) < Math.floor(cost)){
+		dropCoins(Math.floor(cost)-Math.floor(totalCost));
+	}
+
+	totalCost = cost;
+	//Do code Set the cost of the HTML element...
+}
+
+//Make The coins in the movie fall down. starts and pauses the video at the right places.
+function dropCoins(amount){
+	//Do the drop coins code.
+}
+
+//Takes the total amount of watt and converts it to the amount of money it costs.
+function money_converter(totalwatt){
+
+	var priceKWH = document.getElementByID("price").value;
+	var cost = (totalwatt / 1000)*900 * priceKWH;//Vet inte om denna conversion är rätt men lägg en kik.
+	//Do convert code
+	return cost;
+}
+
