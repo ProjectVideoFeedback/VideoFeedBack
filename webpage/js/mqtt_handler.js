d997e654afa6a63fcdbb6e2f9286f6acfcdf0d10
Viktor Yberg <yberg@kth.se>
2015-03-11 12:49:35 +0100

    set daily

--- webpage/js/mqtt_handler.js
+++ webpage/js/mqtt_handler.js
@@ -5,8 +5,7 @@ function setDaily(msg) {
 	if(msg.topic == 'VideoFeedbackDaily') {
 		var payload = msg.payload;
 		console.log("Topic: " + msg.topic + " Payload: " + payload);
-		kwhTotal = payload.accumulated;
-		console.log("total: " + total);
-		$('#kwh-today').html(total + " kwh");
+		console.log("total: " + payload.accumulated);
+		$('#kwh-today').html(payload.accumulated + " kWh");
 	}
 }
