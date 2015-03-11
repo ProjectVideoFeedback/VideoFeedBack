6930ede89223b4a927ae4b4d26c49c55399946e7
Steven Ly <steven.ly@live.se>
2015-03-11 15:06:19 +0100

    mqtt_handler.js
    update...

--- webpage/js/mqtt_handler.js
+++ webpage/js/mqtt_handler.js
@@ -4,16 +4,21 @@ var kwhTotal;
 function setDaily(msg) {
 	if(msg.topic == 'VideoFeedbackDaily') {
 		var payload = msg.payload.split(/,|:/);
-		console.log("Topic: " + msg.topic + " Payload: " + msg.payload);
-		kwhTotal = payload[5].replace(/"/g,'');
-		console.log("total: " + kwhTotal);
-		//var price = &('#price').val;
 		
-		//if(price == undefined)
-			//price = 1.3;
+		if(kwhTotal != payload[5].replace(/"/g,'')){
+			kwhTotal = payload[5].replace(/"/g,'');
+		
+			console.log("Topic: " + msg.topic + " Payload: " + msg.payload);
+			console.log("total: " + kwhTotal);
+		
+			var price = &('#price').val;
+		
+			//if(price == undefined)
+				//price = 1.3;
 			
 			
-		$('#kwh-today').html(kwhTotal + " kWh");
-		$('#money-today').html((kwhTotal*1.3).toFixed(2) + " kr");
+			$('#kwh-today').html(kwhTotal + " kWh");
+			$('#money-today').html((kwhTotal*1.3).toFixed(2) + " kr");
+		}
 	}
 }
