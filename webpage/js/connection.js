2e41bb938e00564e6d7b1fd5c53eb0d83051d7bf
Viktor Yberg <yberg@kth.se>
2015-04-22 21:08:14 +0200

    jump to right time in video on connection

--- webpage/js/connection.js
+++ webpage/js/connection.js
@@ -5,75 +5,39 @@ var start, end, time;
 var startTime = new Date();
 var wattSum = 0;
 var totalCost = 0;
+var connected = false;
 
-function connect(source){
+/* Connect and subscribe to a host */
+function connect(source) {
 	start = new Date().getTime();
 	socket = io.connect(source);
-	socket.on('connect', function () {
-	    socket.on('mqtt', function (msg) {
-				check(msg);
-				setDaily(msg);
+	socket.on('connect', function() {
+	    socket.on('mqtt', function(msg) {
+			check(msg);
+			setDaily(msg);
+			/* ------------------------- <REAL> ------------------------- */
+			/* Jump to time in video where amount of coins is the same as the current cost */
+			/*
+			if (connected === false) {
+				coins = getDailyCost();
+				if (!isNaN(coins)) {
+					connected = true;
+					if (timestamps[getDailyCost()] !== undefined)
+						video.currentTime = timestamps[coins];
+				}
+			}
+			*/
+			/* ------------------------- </REAL> ------------------------- */
 	    });
 	});
-	socket.emit('subscribe',{topic:'/some/sensor/data'});
+	socket.emit('subscribe', { topic:'/some/sensor/data' });
 }
 
-function measurement_storage(msg, date){
-
-	measurement_data.push(
-		{"buildingwatt": msg.payload, "date": date}
-		);
-}
-
-function check(msg){
-	if(msg.topic == 'buildingWatt'){
-
+function check(msg) {
+	if (msg.topic == 'buildingWatt') {
 		end = new Date().getTime();
-		time = end-start;
+		time = end - start;
 		start = end;
 		wattSum += msg.payload;
-
-		//setTotalCost(money_converter(wattSum));
-		//console.log(" KWH: " + (wattSum*((new Date()).getTime() - startTime.getTime())/(3600 * 1000))/1000);
-		//measurement_storage(msg, new Date());
-		//console.log(msg.topic+' '+msg.payload+', time elapsed:'+time+', Date:'+new Date());
-		//console.log("size: " + measurement_data.length + " buildingwatt: " + measurement_data[measurement_data.length-1]["buildingwatt"] + " Date: " + measurement_data[measurement_data.length-1]["date"] + " KWH: " + wattSum/1000);
 	}
-}
-
-//Sets the cost amount. It also checks weather of not the new cost has become greater that the previous total,
-//And starts the drop coins.
-function setTotalCost(cost){
-	if(Math.floor(totalCost) < Math.floor(cost)){
-		dropCoins(Math.floor(cost)-Math.floor(totalCost));
-	}
-
-	totalCost = cost;
-	//Do code Set the cost of the HTML element...
-}
-
-//Make The coins in the movie fall down. starts and pauses the video at the right places.
-function dropCoins(amount){
-	//Do the drop coins code.
-	var video = document.getElementById("video");
-	var nextPause = 100;
-	video.start();
-	video.addEventListener("timeupdate", function(){
-	    if(this.currentTime >=  nextPause) {
-	        this.pause();
-	    }
-	});
-}
-
-//Takes the total amount of watt and converts it to the amount of money it costs.
-function money_converter(totalwatt){
-
-	var priceKWH = document.getElementById("price").value;
-	var cost = 0;
-	if(priceKWH > 0){
-		cost = (totalwatt / (1000*900)) * priceKWH;//Vet inte om denna conversion är rätt men lägg en kik.
-	}else{
-		alert("Enter your kWh price in the settings menu.");
-	}
-	return cost;
 }
