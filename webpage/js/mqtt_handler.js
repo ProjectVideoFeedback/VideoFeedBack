bae21cda21a25932a35df5b1f23f38db97768add
Viktor Yberg <yberg@kth.se>
2015-04-22 21:08:41 +0200

    jump to right time in video on price change

--- webpage/js/mqtt_handler.js
+++ webpage/js/mqtt_handler.js
@@ -1,31 +1,32 @@
 var daily;
 var monthly;
+var price = 1.3;
 
 function setDaily(msg) {
-	if(msg.topic == 'VideoFeedbackDailyMonthly') {
+	if (msg.topic == 'VideoFeedbackDailyMonthly') {
 		var payload = msg.payload.split(/,|:/);
-
-		if(daily != payload[5].replace(/"/g,'')){
+		if (daily != payload[5].replace(/"/g,'')) {
 			daily = payload[5].replace(/"/g,'');
-
 			monthly = payload[7].replace(/"/g,'');
-
 			console.log("Topic: " + msg.topic + " Payload: " + msg.payload);
-			/*console.log("totalD: " + daily);
-			console.log("totalM: " + monthly);*/
-
-			updateStat();
+			updateStats();
 		}
 	}
 }
 
-function updateStat(){
-	var price = $('#price').val();
-	price = price.replace(/,/g,'.');
-	price = parseFloat(price);
-	if(price === undefined)
+function getDailyCost() {
+	return Math.floor(daily * price);
+}
+
+/* Update stats (power consumption) based on price per kWh */
+function updateStats() {
+	price = parseFloat($('#price').val().replace(/,/g,'.'));
+	if (price === undefined)
 		price = 1.3;
 
+	/* If price is changed, jump to the right time */
+	if (timestamps[getDailyCost()] !== undefined)
+		video.currentTime = timestamps[getDailyCost()];
 
 	$('#kwh-today').html(daily + " kWh");
 	$('#money-today').html((daily*price).toFixed(2) + " kr");
